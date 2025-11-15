063c5f267fafa588df883d8621014d4084dcce8e
Viktor Iptyshev <iptvik@gmail.com>
2025-11-16 04:07:16 +0700

    get rid of test handlers

--- app/main.py
+++ app/main.py
@@ -5,18 +5,3 @@ from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
 app.include_router(api_router)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World!"}
-
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
