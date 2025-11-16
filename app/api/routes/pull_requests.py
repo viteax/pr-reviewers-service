1134552a81bab7fb46ffbae69297afee79af6ece
Viktor Iptyshev <iptvik@gmail.com>
2025-11-16 19:19:06 +0700

    fix bug, add autodocumentation

--- app/api/routes/pull_requests.py
+++ app/api/routes/pull_requests.py
@@ -1,8 +1,10 @@
+import random
 from datetime import datetime
 from typing import Annotated
 from zoneinfo import ZoneInfo
 
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, status
+from fastapi.responses import JSONResponse
 
 from app.core.db import db
 from app.exceptions import ConflictException, NotFoundException
@@ -13,13 +15,18 @@ router = APIRouter(prefix="/pullRequest", tags=["PullRequests"])
 
 @router.post(
     "/create",
-    responses={404: {"description": "Пользователь не найден", "content": ""}},
+    status_code=status.HTTP_201_CREATED,
+    response_class=JSONResponse,
+    responses={
+        status.HTTP_404_NOT_FOUND: {"description": "user/team not found"},
+        status.HTTP_409_CONFLICT: {"description": "pr already exists"},
+    },
 )
 async def create_pull_request(
     pull_request_id: Annotated[str, Body()],
     pull_request_name: Annotated[str, Body()],
     author_id: Annotated[str, Body()],
-):
+) -> JSONResponse:
     author = db.get_user_or_raise_not_found(author_id)
     team = db.get_team_or_raise_not_found(author.team_name)
 
@@ -27,7 +34,7 @@ async def create_pull_request(
         raise ConflictException(code=ErrorCode.PR_EXISTS, message="PR already exists")
 
     assigned_reviewers = []
-    for member in team.members:
+    for member in random.sample(team.members, len(team.members)):
         if member.is_active and member.user_id != author.user_id:
             assigned_reviewers.append(member.user_id)
         if len(assigned_reviewers) == 2:
@@ -41,16 +48,17 @@ async def create_pull_request(
         assigned_reviewers=assigned_reviewers,
     )
     db.pull_requests[pr.pull_request_id] = pr
-    return {"pr": pr.model_dump(exclude_none=True)}
+    return JSONResponse(
+        content={"pr": pr.model_dump(exclude_none=True, mode="json")},
+        status_code=status.HTTP_201_CREATED,
+    )
 
 
 @router.post(
     "/merge",
-    responses={404: {"description": "Пользователь не найден", "content": ""}},
+    responses={status.HTTP_404_NOT_FOUND: {"description": "pr not found"}},
 )
-async def assign_merged(
-    pull_request_id: Annotated[str, Body()],
-):
+async def assign_merged(pull_request_id: Annotated[str, Body(embed=True)]):
     pr = db.get_pull_request_or_raise_not_found(pull_request_id)
     if pr.status == PullRequestStatus.OPEN:
         pr.merged_at = datetime.now(ZoneInfo("Europe/Moscow"))
@@ -58,7 +66,13 @@ async def assign_merged(
     return {"pr": pr}
 
 
-@router.post("/reassign")
+@router.post(
+    "/reassign",
+    responses={
+        status.HTTP_404_NOT_FOUND: {"description": "user/team not found"},
+        status.HTTP_409_CONFLICT: {"description": "domain data assign conflict"},
+    },
+)
 async def reassign_reviewer(
     pull_request_id: Annotated[str, Body()],
     old_user_id: Annotated[str, Body()],
@@ -86,7 +100,9 @@ async def reassign_reviewer(
 
     replaced_by: str | None = None
     for member in team.members:
-        if member.user_id not in pr.assigned_reviewers and member.is_active:
+        if member.user_id == pr.author_id or not member.is_active:
+            continue
+        elif member.user_id not in pr.assigned_reviewers:
             replaced_by = member.user_id
             pr.assigned_reviewers[pr.assigned_reviewers.index(old_user_id)] = (
                 replaced_by
