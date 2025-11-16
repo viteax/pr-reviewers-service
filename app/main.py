a668dbae9ea821d44cc4c0e063dd064deb098927
Viktor Iptyshev <iptvik@gmail.com>
2025-11-16 19:19:36 +0700

    change version to stable 1.0.0

--- app/main.py
+++ app/main.py
@@ -2,5 +2,8 @@ from fastapi import FastAPI
 
 from app.api.main import api_router
 
-app = FastAPI(title="PR Reviewers Service")
+app = FastAPI(
+    title="PR Reviewers Service",
+    version="1.0.0",
+)
 app.include_router(api_router)
