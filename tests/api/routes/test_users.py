574f34133d6e746498d095caede8699637ead57b
Viktor Iptyshev <iptvik@gmail.com>
2025-11-18 22:17:20 +0700

    add users api test

--- tests/api/routes/test_users.py
+++ tests/api/routes/test_users.py
@@ -1,3 +1,4 @@
+from fastapi import status
 from fastapi.testclient import TestClient
 
 from app.core.db import DataBase
@@ -21,6 +22,34 @@ def test_set_is_active(client: TestClient, fake_db: DataBase) -> None:
     assert user.is_active == is_active
 
 
+def test_set_is_active_validation_error(client: TestClient, fake_db: DataBase) -> None:
+    user_id = "u3"
+    is_active = "323"
+
+    r = client.post(
+        "/users/setIsActive",
+        json={
+            "user_id": user_id,
+            "is_active": is_active,
+        },
+    )
+    assert r.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+
+
+def test_set_is_active_not_found_error(client: TestClient, fake_db: DataBase) -> None:
+    user_id = "non_exist_user"
+    is_active = "1"
+
+    r = client.post(
+        "/users/setIsActive",
+        json={
+            "user_id": user_id,
+            "is_active": is_active,
+        },
+    )
+    assert r.status_code == status.HTTP_404_NOT_FOUND
+
+
 def test_get_review(client: TestClient, fake_db: DataBase) -> None:
     user_id = "u162"
     r = client.get("/users/getReview", params={"user_id": user_id})
@@ -34,6 +63,21 @@ def test_get_review(client: TestClient, fake_db: DataBase) -> None:
             correct_pull_requests.append(
                 PullRequestShort(**pr.model_dump()).model_dump()
             )
+
+    assert us_id == user_id
+    assert pull_requests == correct_pull_requests
+    assert pull_requests != []
+
+
+def test_get_review_not_exist_user(client: TestClient, fake_db: DataBase) -> None:
+    user_id = "not_exist_user_id"
+    correct_pull_requests = []
+
+    r = client.get("/users/getReview", params={"user_id": user_id})
+    data = r.json()
+
+    us_id = data["user_id"]
+    pull_requests = data["pull_requests"]
 
     assert us_id == user_id
     assert pull_requests == correct_pull_requests
